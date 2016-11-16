9d0f983ea5955395ef98db8fb603fe9ba6e19ae5
Nathan Chancellor <natechancellor@gmail.com>
2016-11-16 09:09:40 -0700

    Core: wrap sdclang commands with ccache
    As far as I am aware (and through testing of my own), sdclang commands are not getting wrapped for ccache.
    
    Reason for this is in the private variable assignment section, when my_cc is stripped, it is not empty, so a wrapper is never assigned. Since it is not going to be touched until the PRIVATE_CC or PRIVATE_CXX variable assignment, we can just do it here.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- core/binary.mk
+++ core/binary.mk
@@ -526,12 +526,19 @@ my_target_global_ldflags := $($(LOCAL_2ND_ARCH_VAR_PREFIX)CLANG_TARGET_GLOBAL_LD
         my_target_global_cflags += $(SDCLANG_COMMON_FLAGS) $(SDCLANG_PRECONFIGURED_FLAGS)
         SDCLANG_PRECONFIGURED_FLAGS :=
 
-        ifeq ($(strip $(my_cc)),)
-            my_cc := $(SDCLANG_PATH)/clang
-        endif
-        ifeq ($(strip $(my_cxx)),)
-            my_cxx := $(SDCLANG_PATH)/clang++
-        endif
+        SDCLANG_PRECONFIGURED_FLAGS += -fuse-ld=qcld -flto
+        my_target_global_ldflags += -fuse-ld=qcld -flto $(LOCAL_SDCLANG_LTO_LDFLAGS)
+      endif
+    endif
+
+    my_target_global_cflags += $(SDCLANG_COMMON_FLAGS) $(SDCLANG_PRECONFIGURED_FLAGS)
+    SDCLANG_PRECONFIGURED_FLAGS :=
+
+    ifeq ($(strip $(my_cc)),)
+      my_cc := $(my_cc_wrapper) $(SDCLANG_PATH)/clang
+    endif
+    ifeq ($(strip $(my_cxx)),)
+      my_cxx := $(my_cxx_wrapper) $(SDCLANG_PATH)/clang++
     endif
 else
 my_target_global_cflags := $($(LOCAL_2ND_ARCH_VAR_PREFIX)TARGET_GLOBAL_CFLAGS)
